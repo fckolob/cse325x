64ca7d95f9eca06b3aeb4022726858dc87ef3924
fckolob <131497920+fckolob@users.noreply.github.com>
2026-05-02 21:04:07 -0300

    initial commit

---
+++ firstApp/MyConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+DateTime christmas = new DateTime(2026, 12, 25);
+TimeSpan diference = christmas - DateTime.Now;
+Console.WriteLine($"There are {diference.Days} days until the next Christmas");
